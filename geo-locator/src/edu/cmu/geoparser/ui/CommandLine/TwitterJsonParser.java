332ebfd28b619f63cae0671fa81644b4dd2bbb68
weizh <wynnzh@gmail.com>
2013-10-25 11:28:39 -0400

    add jsonTweets sample file, and a new TwitterJsonParser, a sample Json tweet parser.

---
+++ geo-locator/src/edu/cmu/geoparser/ui/CommandLine/TwitterJsonParser.java
@@ -0,0 +1,88 @@
+package edu.cmu.geoparser.ui.CommandLine;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+
+import edu.cmu.geoparser.Disambiguation.ContextDisamb;
+import edu.cmu.geoparser.io.GetReader;
+import edu.cmu.geoparser.model.Tweet;
+import edu.cmu.geoparser.parser.english.EnglishParser;
+import edu.cmu.geoparser.resource.trie.IndexSupportedTrie;
+import twitter4j.Status;
+import twitter4j.TwitterException;
+import twitter4j.json.DataObjectFactory;
+
+public class TwitterJsonParser {
+
+  public static void main(String argv[]) throws IOException {
+
+    /**
+     * Preparation: initialize the parser.
+     */
+    String uri = "/Users/indri/Eclipse_workspace/";
+    String geonames = uri + "GeoNames/cities1000.txt";
+    String gazindex = uri + "GazIndex";
+    IndexSupportedTrie topotrie = new IndexSupportedTrie(geonames, gazindex, true, false);
+    EnglishParser enparser = new EnglishParser("res/", topotrie, false);
+
+    ContextDisamb c = new ContextDisamb();
+
+    /**
+     * read the tweets, one Json tweet per line.
+     * 
+     */
+    BufferedReader br = GetReader.getUTF8FileReader("SampleInput/jsonTweets.txt");
+    String line = null;
+    while ((line = br.readLine()) != null) {
+
+      // parse the tweet json file into a Status object( twitter4j convention).
+      Status rawTweet = null;
+      try {
+        rawTweet = DataObjectFactory.createStatus(line.trim());
+      } catch (TwitterException e) {
+        System.err.println("JSON File is corrupted. Continue");
+        /**
+         * Do something to handle this problem. Ignore it, or output the JSON.
+         */
+        continue;
+      }
+
+      /**
+       *  wrap it into our tweet object. Here we only copy the text.
+       */
+      Tweet aTweet = new Tweet();
+      aTweet.setText(rawTweet.getText());
+
+      
+      /**
+       *  The next several lines are identical to CmdInputParser.java
+       */
+
+      List<String> match = enparser.parse(aTweet);
+
+      if (match == null || match.size() == 0) {
+        System.out.println("No toponyms in text. Do something, then continue.");
+        continue;
+      } else { // if matches are found:
+        HashSet<String> reducedmatch = new HashSet<String>();
+        for (String s : match)
+          reducedmatch.add(s.substring(3, s.length() - 3));
+
+        HashMap<String, String[]> result = c.returnBestTopo(topotrie, reducedmatch);
+
+        if (result == null) {
+          System.out.println("No GPS for any location is found.");
+        } else {
+          System.out.println("The grounded location(s) are: (topo: country_state, latitude, longitude.");
+          for (String topo : result.keySet())
+            System.out.println(topo + ": " + result.get(topo)[2] + " " + result.get(topo)[0] + " "
+                    + result.get(topo)[1]);
+        }
+
+      }
+    }
+  }
+}
